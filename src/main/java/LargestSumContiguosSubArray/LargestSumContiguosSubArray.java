dfa4c5031b64ca2bc6bf2d9102124763a32c95b9
I354348 <nishant.joshi01@sap.com>
2020-02-21 19:49:16 +0530

    Largest contiguos subbary solution

---
+++ src/main/java/LargestSumContiguosSubArray/LargestSumContiguosSubArray.java
@@ -0,0 +1,47 @@
+package LargestSumContiguosSubArray;
+
+import java.math.BigInteger;
+
+public class LargestSumContiguosSubArray {
+
+
+
+
+
+    private  static BigInteger calculate(BigInteger[] input){
+
+        BigInteger max_so_far=input[0],max_ending_here=input[0];
+        int startIndex=0,endIndex=0;
+        for(int i=1;i<input.length;i++){
+
+            if((max_ending_here.add(input[i])).compareTo(input[i])==-1){
+                max_ending_here=input[i];
+                startIndex=i;
+                endIndex=i;
+            }else{
+                max_ending_here=max_ending_here.add(input[i]);
+            }
+
+
+            if(max_so_far.compareTo(max_ending_here)==-1){
+                max_so_far=max_ending_here;
+                endIndex=i;
+            }
+
+        }
+
+        System.out.println(" LargestSumContiguosSubArray starts and ends with index - "+startIndex+" , "+endIndex);
+
+        return max_so_far;
+    }
+
+
+    public static void main(String[] args){
+
+
+        BigInteger[] input={new BigInteger("-2"),new BigInteger("-3"),new BigInteger("4"),new BigInteger("-1"),new BigInteger("-2"),new BigInteger("1"),new BigInteger("5"),new BigInteger("-3")};
+
+        BigInteger result=calculate(input);
+        System.out.println(" Max sum of LargestSumContiguosSubArray -  "+result);
+    }
+}
---
+++ src/main/java/LargestSumContiguosSubArray/LargestSumContiguosSubArrayDP.java
@@ -0,0 +1,40 @@
+package LargestSumContiguosSubArray;
+
+import java.math.BigInteger;
+
+public class LargestSumContiguosSubArrayDP {
+
+
+    private static BigInteger[] createlookup(int size){
+        BigInteger[] lookup=new BigInteger[size];
+        lookup[0]=BigInteger.ZERO;
+        return  lookup;
+    }
+
+
+
+    private  static BigInteger calculate(BigInteger[] input,BigInteger[] lookup){
+
+        for(int i=1;i<input.length+1;i++){
+            lookup[i]=BigInteger.valueOf(Math.max((lookup[i-1].add(input[i-1])).intValue(),input[i-1].intValue()));
+        }
+
+        BigInteger maxValue=lookup[1];
+        for(int i=2;i<input.length+1;i++){
+            if(lookup[i].compareTo(maxValue)==1){
+                maxValue=lookup[i];
+            }
+        }
+        return maxValue;
+    }
+
+
+    public static void main(String[] args){
+
+
+        BigInteger[] input={new BigInteger("-2"),new BigInteger("-3"),new BigInteger("4"),new BigInteger("-1"),new BigInteger("-2"),new BigInteger("1"),new BigInteger("5"),new BigInteger("-3")};
+        BigInteger[] lookup=createlookup(input.length+1);
+        BigInteger result=calculate(input,lookup);
+        System.out.println(" Max sum of LargestSumContiguosSubArray -  "+result);
+    }
+}
